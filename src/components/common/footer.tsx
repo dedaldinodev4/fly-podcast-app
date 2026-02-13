7b2bdf9018a49a61d216bf77074dd6c083f93863
Dedaldino Daniel <dedaldinodev4@gmail.com>
2026-02-13 13:37:03 +0100

    feat: add footer component

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,9 @@
+
+
+export const Footer = () => {
+  return (
+    <footer className="py-10 text-center text-gray-500 text-sm border-t border-white/10">
+      © 2026 Fly Podcast. Todos os direitos reservados.
+    </footer>
+  )
+}
